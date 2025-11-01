d89c1380022e36301b7e4ec9f02a3346f5749f9c
Shoeb Shaikh <shoeb.sn1125@gmail.com>
2025-11-01 19:27:25 +0530

    Remove sensitive client credentials from provider configuration

--- data-sources-block-practise/provider.tf
+++ data-sources-block-practise/provider.tf
@@ -1,9 +1,9 @@
+# Add your service principle credentials accordingly
 provider "azurerm" {
   features {}
   subscription_id = "16112944-fd51-430f-bfb9-9d32892257ad"
   tenant_id       = "f071f990-4dfc-42b8-a903-d40289469da7"
-  client_id       = "e5308052-ad75-4a01-bd7f-46a36fefe1c4"
-  client_secret   = "4dZ8Q~~5wGtYTOJehVzRpQo-gI~liQHMva4tEbKk"
+
 }
 
 
